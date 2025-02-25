3b96bae6662bbf8489f548eb064ed4163af3db3e
Gilson Oliveira <gilsonsilva041327@gmail.com>
2025-02-25 15:28:11 -0300

    atualizei app

--- portifolio/script.js
+++ portifolio/script.js
@@ -24,3 +24,46 @@ const abrirMenu = () => {
 }
 
 menuBurguer.addEventListener('click',abrirMenu);
+
+
+
+const typingEffect = document.getElementById('typingEffect');
+const typingEffect2 = document.getElementById('typingEffect2');
+const textToTyping = "Hi, I'm Gilson, Front-end Developer.";
+const textToTyping2 = "Html / CSS / JavaScript";
+const buttons = document.querySelector('.buttons');
+let index1 = 0;
+let index2 = 0;
+
+function typeWritter() {
+    // Digitar o primeiro texto
+    if(index1 < textToTyping.length){
+        typingEffect.innerHTML += textToTyping.charAt(index1);
+        index1++;
+        setTimeout(typeWritter, 50); // Continua a digitação do primeiro texto
+    }
+    else {
+        // Quando o primeiro texto acabar, chama a função para o segundo texto
+        setTimeout(typeWritter2, 300);  // Dê um pequeno delay antes de começar o segundo texto
+    }
+}
+
+function typeWritter2() {
+    // Digitar o segundo texto
+    if(index2 < textToTyping2.length) {
+        typingEffect2.innerHTML += textToTyping2.charAt(index2);
+        index2++;
+        setTimeout(typeWritter2, 50); // Continua a digitação do segundo texto
+    }
+    else{
+        setTimeout(() => {
+            buttons.classList.add('show-buttons')
+        }, 300);
+
+    }   
+}
+
+// Iniciar o efeito de digitação ao carregar a página
+window.onload = () => {
+    typeWritter();
+};
