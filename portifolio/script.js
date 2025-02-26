92355eaee4c67ffccd62804a6541d9ab4d5f2c41
GilsonOliveeira <gilsonsilva041327@gmail.com>
2025-02-25 22:17:51 -0300

    att

--- portifolio/script.js
+++ portifolio/script.js
@@ -28,9 +28,8 @@ menuBurguer.addEventListener('click',abrirMenu);
 
 
 const typingEffect = document.getElementById('typingEffect');
-const typingEffect2 = document.getElementById('typingEffect2');
+const icons = document.getElementById('icons');
 const textToTyping = "Hi, I'm Gilson, Front-end Developer.";
-const textToTyping2 = "Html / CSS / JavaScript";
 const buttons = document.querySelector('.buttons');
 let index1 = 0;
 let index2 = 0;
@@ -48,20 +47,13 @@ function typeWritter() {
     }
 }
 
-function typeWritter2() {
-    // Digitar o segundo texto
-    if(index2 < textToTyping2.length) {
-        typingEffect2.innerHTML += textToTyping2.charAt(index2);
-        index2++;
-        setTimeout(typeWritter2, 50); // Continua a digitação do segundo texto
-    }
-    else{
-        setTimeout(() => {
-            buttons.classList.add('show-buttons')
-        }, 300);
+function typeWritter2(){
+    setTimeout(() => {
+        icons.style.display = 'flex'; // Exibe os ícones
+        buttons.classList.add('show-buttons');
 
-    }   
-}
+    }, 100);
+};
 
 // Iniciar o efeito de digitação ao carregar a página
 window.onload = () => {
